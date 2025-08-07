70aabbb8feb2ac7daadba85d49e8b779b2588c30
Guybrush3791 <tech@methodwolf.com>
2025-08-07 11:23:02 +0200

    LC

--- src/main/java/com/booleanuk/core/CohortManager.java
+++ src/main/java/com/booleanuk/core/CohortManager.java
@@ -1,5 +1,18 @@
 package com.booleanuk.core;
 
+import java.util.List;
+
+//    The work is not done when you can't add anything
+//    but is done when you can't remove anything
+
 public class CohortManager {
 
+    // Green stage
+
+    public Boolean search(List<String> cohorts, String name) {
+
+        return cohorts == null || name == null || name.isBlank()
+                ? null
+                : cohorts.contains(name);
+    }
 }
--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -3,6 +3,65 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class CohortManagerTest {
 
+    // Red stage
+
+    @Test
+    public void testSearchEmptyName() {
+
+        List<String> cohorts = List.of(
+                "cohort java 1",
+                "cohort java 2",
+                "cohort java 3"
+        );
+        CohortManager cm = new CohortManager();
+
+        Assertions.assertEquals(null, cm.search(cohorts, null));
+        Assertions.assertEquals(null, cm.search(cohorts, ""));
+        Assertions.assertEquals(null, cm.search(cohorts, "    "));
+        Assertions.assertEquals(null, cm.search(cohorts, "\n\n"));
+        Assertions.assertEquals(null, cm.search(cohorts, "\t\n"));
+    }
+
+    @Test
+    public void testSearchEmptyCohortList() {
+
+        List<String> cohorts = new ArrayList<>();
+        CohortManager cm = new CohortManager();
+
+        Assertions.assertEquals(null, cm.search(null, "valid name"));
+        Assertions.assertEquals(false, cm.search(cohorts, "valid name"));
+    }
+
+    @Test
+    public void testSearchNameInList() {
+
+        List<String> cohorts = List.of(
+                "cohort java 1",
+                "cohort java 2",
+                "cohort java 3"
+        );
+        CohortManager cm = new CohortManager();
+
+        Assertions.assertEquals(true, cm.search(cohorts, "cohort java 1"));
+        Assertions.assertEquals(true, cm.search(cohorts, "cohort java 3"));
+    }
+
+    @Test
+    public void testSearchNameNotInList() {
+
+        List<String> cohorts = List.of(
+                "cohort java 1",
+                "cohort java 2",
+                "cohort java 3"
+        );
+        CohortManager cm = new CohortManager();
+
+        Assertions.assertEquals(false, cm.search(cohorts, "cohort-java 1"));
+        Assertions.assertEquals(false, cm.search(cohorts, "cohort java 4"));
+    }
 }
