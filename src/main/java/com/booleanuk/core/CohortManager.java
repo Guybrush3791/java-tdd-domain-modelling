fdf12e1c12f64b574c9904974ea001e9f2c82f99
Guybrush3791 <tech@methodwolf.com>
2025-08-14 10:35:59 +0200

    live code

--- src/main/java/com/booleanuk/core/CohortManager.java
+++ src/main/java/com/booleanuk/core/CohortManager.java
@@ -1,5 +1,19 @@
 package com.booleanuk.core;
 
+import java.util.List;
+
 public class CohortManager {
 
+
+    public Boolean search(List<String> cohorts, String name) {
+
+        if (cohorts == null || name == null || cohorts.isEmpty() || name.isBlank())
+            return null;
+
+        for (String cohort : cohorts)
+            if (cohort.toLowerCase().trim().contains(name.toLowerCase().trim()))
+                return true;
+
+        return false;
+    }
 }
--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -3,6 +3,43 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class CohortManagerTest {
 
+    List<String> cohorts = List.of("PHP Cohort", "Java Cohort", "JS Cohort");
+
+
+    @Test
+    public void getTrueIfNameIsInList() {
+
+        String cohortName = "php";
+
+        CohortManager cm = new CohortManager();
+        Assertions.assertEquals(true, cm.search(cohorts, cohortName));
+    }
+
+    @Test
+    public void getFalseIfNameIsNotInList() {
+
+        String cohortName = "Perl";
+
+        CohortManager cm = new CohortManager();
+        Assertions.assertEquals(false, cm.search(cohorts, cohortName));
+    }
+
+    @Test
+    public void getNullIfCohortListOrCohortNameIsBlank() {
+
+        CohortManager cm = new CohortManager();
+
+        String cohortName = " ";
+        Assertions.assertNull(cm.search(cohorts, cohortName));
+
+        cohortName = null;
+        Assertions.assertNull(cm.search(cohorts, cohortName));
+
+        cohortName = "php";
+        Assertions.assertNull(cm.search(null, cohortName));
+    }
 }
